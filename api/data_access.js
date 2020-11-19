63a1ad711502911ae5117fa3144a2be575fdf39c
Alan Tsui <alantsui18@gmail.com>
2020-11-18 19:03:47 -0800

    formatted stuff

--- api/data_access.js
+++ api/data_access.js
@@ -1,6 +1,6 @@
-/** 
+/**
  * Muse - Database access methods
- * 
+ *
  * This file is to store all the database access methods
  * This will include profile creating and reading, user creating and reading,
  * etc.
@@ -8,31 +8,36 @@
 
 /**
  * Creates a user based on the user's Spotify information into the database
- * that's passed in. 
- * 
+ * that's passed in.
+ *
  * @param {*} db reference to the database to add to
  * @param {*} email email of the user
  * @param {*} displayName name of the user
  * @param {*} spotifyID Spotify ID of the user
- * @param {*} refreshToken Spotify refresh token of the user 
+ * @param {*} refreshToken Spotify refresh token of the user
  */
-export async function createUser(db, email, displayName, spotifyID, refreshToken) {
-
-    // create a data document to be stored 
+export async function createUser(
+  db,
+  email,
+  displayName,
+  spotifyID,
+  refreshToken
+) {
+  // create a data document to be stored
   const userData = {
-    name: displayName, 
+    name: displayName,
     spotify_id: spotifyID,
-    refresh_token: refreshToken, 
+    refresh_token: refreshToken,
     location: [],
     profile: email, // hash function based on refresh token
     friends: [],
     messages: [],
     stats_id: email,
-    in_harmony: email // hash function based on refresh token
-  }
+    in_harmony: email, // hash function based on refresh token
+  };
 
   // go into the user tab and create the user
-  const res = await db.collection('user').doc(email).set(userData);
+  const res = await db.collection("user").doc(email).set(userData);
 
   // create user profile
   createUserProfile(db, email);
@@ -45,8 +50,7 @@ export async function createUser(db, email, displayName, spotifyID, refreshToken
   // TODO: check what the response is and decide where to go from here
 
   // log to console the result
-  console.log('Added', res);
-  
+  console.log("Added", res);
 }
 
 /**
@@ -55,23 +59,22 @@ export async function createUser(db, email, displayName, spotifyID, refreshToken
  * @param {*} email email of the user
  */
 export async function createUserProfile(db, email) {
-
   // create data object to be stored
   const userProfileData = {
-    profile_picture: '',
-    biography: '',
-    profile_url: '',
+    profile_picture: "",
+    biography: "",
+    profile_url: "",
     select_playlist: [],
-    social_media: []
-  }
+    social_media: [],
+  };
 
   // go into the profile tab and create the profile for the user
-  const res = await db.collection('profile').doc(email).set(userProfileData);
+  const res = await db.collection("profile").doc(email).set(userProfileData);
 
   // TODO: check what response is and decide where to go from here
 
   // log to console the result
-  console.log('Added', res); 
+  console.log("Added", res);
 }
 
 /**
@@ -80,28 +83,28 @@ export async function createUserProfile(db, email) {
  * @param {*} email email of the user
  * @param {*} refresh_token Spotify refresh token of the user
  */
-export async function createUserStats(db, email, refresh_token) {
-
-}
+export async function createUserStats(db, email, refresh_token) {}
 
 /**
- * Creates the user's in harmony list from current 
- * @param {*} db 
- * @param {*} email 
- * @param {*} refresh_token 
+ * Creates the user's in harmony list from current
+ * @param {*} db
+ * @param {*} email
+ * @param {*} refresh_token
  */
 export async function createUserInHarmony(db, email, refresh_token) {
-  
   // TODO: in harmony algorithm
 
   // Create the in harmony list (empty for now)
   const userInHarmonyData = {
-    similar_users: []
-  }
+    similar_users: [],
+  };
 
   // go into in harmony tab and create the profile for the user
-  const res = await db.collection('in_harmony').doc(email).set(userInHarmonyData);
+  const res = await db
+    .collection("in_harmony")
+    .doc(email)
+    .set(userInHarmonyData);
 
   // Log to console the result
-  console.log('Added', res);
+  console.log("Added", res);
 }
