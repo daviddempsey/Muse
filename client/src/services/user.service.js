3849425a7275a804cfc6a77424b8659d357f4f8d
David Dempsey <davidgdempsey@gmail.com>
2020-11-18 19:29:10 -0800

    User service created

---
+++ client/src/services/user.service.js
@@ -0,0 +1,31 @@
+import firebase from "../firebase";
+
+const db = firebase.collection("/user");
+
+class UserService {
+  getAll() {
+    return db;
+  }
+
+  getUser(email) {
+    return db.doc(email);
+  }
+
+  getProfilePicture(email) {
+      return db.doc(email).get("profile_picture");
+  }
+
+  create(user) {
+    return db.add(user);
+  }
+
+  update(email, value) {
+    return db.doc(email).update(value);
+  }
+
+  delete(email) {
+    return db.doc(email).delete();
+  }
+}
+
+export default new TutorialDataService();
