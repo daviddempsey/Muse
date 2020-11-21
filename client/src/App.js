e385990f22613800438f08f8583534b7f9fe378c
William Duan <38791932+duanwilliam@users.noreply.github.com>
2020-11-20 21:46:12 -0800

    reorganize file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Axios from "axios";
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import './styles/index.css';
 
 function App() {
-  Axios({
-    method: "GET",
-    url: "http://localhost:5000/",
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(res => {
-    console.log(res.data.message);
-  });
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
