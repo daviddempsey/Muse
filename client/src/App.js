c051e1ba1659f9ed02fb10c5f3a879b380efc750
Edward Yau <edyau@ucsd.edu>
2020-11-22 21:30:24 -0800

    Fixed compiling issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import './styles/index.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import "./styles/index.css";
 
 function App() {
   return (
