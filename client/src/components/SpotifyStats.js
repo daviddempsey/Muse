c47b39237aa62eaf07ed675b06bf81e74018d7f8
Edmond choi <e2choi@ucsd.edu>
2020-11-24 17:50:49 -0800

    stuffff

--- client/src/components/SpotifyStats.js
+++ client/src/components/SpotifyStats.js
@@ -1,47 +1,20 @@
 import React from 'react';
 //import UserService from "../services/user.service";
 
-export class SpotifyStats extends React.Component {
-  constructor(props) {
-    super(props);
+// grabs the spotify stats for a user from firebase and returns it
+export function SpotifyStats() {
+  // commmented out until we implement userservice
+  /*let top_artists = UserService.getTopArtists();
+  let top_genres = UserService.getTopGenres();
+  let top_tracks = UserService.getTopTracks(); */
 
-    this.state = {
-      top_artists: '',
-      top_genres: '',
-      top_tracks: '',
-    };
-  }
-  //checks if component mounted
-  componentDidMount() {
-    this.getSpotifyStats();
-  }
-  // calls to get data from database
-  getSpotifyStats() {
-    /*
-                //calls userservice to return data
-                let dbTopArtists = UserService.getTop_artists();
-                let dbTopGenres = UserService.getTopGenres();
-                let dbTopTracks = UserService.getTopTracks();*/
+  let top_artists = "test top artist text";
+  let top_artists = "test top artist text";
+  let top_artists = "test top artist text";
+  return <div>
+    <h1>{top_artists}</h1>
+    <h1>{top_genres}</h1>
+    <h1>{top_tracks}</h1>
+  </div>
+};
 
-    this.setState({
-      // I am not sure what the database returns is it in array form already?
-      // if it is an array not sure if I'm suppose to map a key to the <li>
-      // also not sure how to go about doing that if we need to.
-      // if we list top artists as an unordered list then rn is fine.
-      // top_artists: dbTopArtists,
-      // top_genres: dbTopGenres,
-      // top_tracks: dbTopTracks,
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h1> Top Artist </h1> <ul> {this.state.top_artists} </ul>{' '}
-        <h1> Top Genres </h1> <ul> {this.state.top_genres} </ul>{' '}
-        <h1> Top Tracks </h1> <ul> {this.state.top_tracks} </ul>{' '}
-      </div>
-    );
-  }
-}
-
-export default SpotifyStats;
