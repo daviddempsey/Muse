f6a827edee55f4f88f4477194bc4e693ca25d12b
William Duan <38791932+duanwilliam@users.noreply.github.com>
2020-11-24 23:47:09 -0800

    static landing page

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './index.css';
 
-import LandingPageHeader from './LandingPageHeader';
+import DefaultLayout from '../DefaultLayout';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-import Footer from '../../components/Footer';
 
 const LandingPage = () => {
   return (
     <div className="LandingPage">
-      <LandingPageHeader/>
-      <Home />
-      <About />
-      <Contact />
-      <Footer />
+      <DefaultLayout>
+        <Home />
+        <About />
+        <Contact />
+      </DefaultLayout>
     </div>
   );
 };
