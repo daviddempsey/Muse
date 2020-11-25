1e69e71a83ac69787ba509ce1df466b5670c95cf
Alan Tsui <alantsui18@gmail.com>
2020-11-24 17:27:51 -0800

    camel?

--- client/src/components/Biography.js
+++ client/src/components/Biography.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import UserService from '../services/user.service';
+//import UserService from '../services/user.service';
 
 /*update a user's biography.
   Current functionality: basic text element with a save button to 
   handle updating firestore database in the future*/
 
- 
-export function Biography() {
-    // uncomment userservice once we get it to work.
-    //let bioText = UserService.getBiography();
-    let bioText = "test text";
-    return <div>{bioText}</div>
-};
+export default function Biography() {
+  // uncomment userservice once we get it to work.
+  //let bioText = UserService.getBiography();
+  let bioText = 'test text';
+  return <div> {bioText} </div>;
+}
--- client/src/components/ProfileLink.js
+++ client/src/components/ProfileLink.js
@@ -1,29 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import UserService from "../services/user.service";
 
-class ProfileLink extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      link: '',
-    };
-  }
-  componentDidMount() {
-    this.getProfileLink();
-  }
-  getProfileLink() {
-    //let profileLinkUrl = UserService.getProfileLink();
-    //this.setState({ link: profileLinkUrl });
-  }
-
-  render() {
-    return (
-      <div>
-        <p> {this.state.link} </p>{' '}
-      </div>
-    );
-  }
+export default function ProfileLink() {
+  // let link = UserService.getProfileLink();
+  let link = 'https://muse.com/test_text';
+  return <div>{link}</div>;
 }
-
-export default ProfileLink;
