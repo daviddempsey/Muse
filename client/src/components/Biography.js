825b5ca825069b60a44c93ab7e4c247ea04e78b3
Edmond choi <e2choi@ucsd.edu>
2020-11-24 16:48:21 -0800

    rewrote biography.js in functional react

--- client/src/components/Biography.js
+++ client/src/components/Biography.js
@@ -1,38 +1,12 @@
 import React from 'react';
-// import UserService from "../services/user.service";
+import userservice from '../services/user.service';
 
 /*update a user's biography.
   Current functionality: basic text element with a save button to 
   handle updating firestore database in the future*/
-class Biography extends React.Component {
-  //constructor
-  constructor(props) {
-    super(props);
-    // set default state of text element instead text box
-    this.state = {
-      Biography: 'Tell us a little bit about yourself :)',
-    };
-  }
 
-  //checks if component mounted
-  componentDidMount() {
-    this.getBiography();
-  }
-
-  getBiography() {
-    //var bioText = UserService.getBiography();
-    var bioText = 'this is a test';
-    this.setState({ Biography: bioText });
-  }
-
-  /* render text box*/
-  render() {
-    return (
-      <div>
-        <h1>{this.state.Biography}</h1>
-      </div>
-    );
-  }
-}
-
-export default Biography;
+ 
+export function Biography() {
+    let bioText = userservice.getBiography();
+    return <div>{bioText}</div>
+};
