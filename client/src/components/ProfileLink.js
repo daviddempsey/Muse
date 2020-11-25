a5ab457f1bddeb461aef8838dbab9d4bcbdbddb5
Edward Yau <edyau@ucsd.edu>
2020-11-25 15:11:37 -0800

    Created file

---
+++ client/src/components/ProfileLink.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+//import UserService from "../services/user.service";
+
+const ProfileLink = () => {
+  // let link = UserService.getProfileLink();
+  //check if component mounted
+  React.useEffect(() => {
+    console.log("Hi");
+  }, []);
+
+  let link = "https://muse.com/test_text";
+  return <div> {link} </div>;
+};
+
+export default ProfileLink;
