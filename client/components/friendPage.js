bb09346dc61169f0f095501d2d8d4cec267a2462
enochli5907 <enochli5907@gmail.com>
2020-11-27 15:37:12 -0800

    Refactored FriendPage

--- client/components/friendPage.js
+++ client/components/friendPage.js
@@ -1,60 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import UserService from "../services/user.service"
 
+/* Make friends list a functional component
+ * Have a friendsPage component that 
 
-/* view and update a user's friends list */
-export default class friendPage extends Component {
-    constructor(props) {
-        super(props);
-        // set default state of friends element to a list containing dict of userEmails
-        this.state = { friends: [
-            {userEmail: ""},
-        ] }
+/* view and a user's friends list */
+export default function friendPage() {
+    // get the friends list from the database
+    let friendsList = UserService.getFriends();
 
-    // event handlers for when we update friends list
-    this.handleAddFriend = this.handleAddFriend.bind(this);
-    this.handleRemoveFriend = this.handleRemoveFriend.bind(this);
-    }
+    const friends = friendsList.map((email, compatibility) => {
+        return <li><Friend email={email, compatibility}/></li>
+    });
+    
+    return (
+        <div>
+            {friends}
+        </div> 
+    )
 
-    // function for viewing a friends page
-    viewFriend(userEmail) {
-        let friend = UserService.getUser(userEmail);
-        return friend;
-    }
 
-    /* event handler for adding a new friend */
-    handleAddFriend (name) {
-        console.log("Added " + name);
-        // add the friend to the user's friends list
-        this.setState((name) => {
-            return {
-                friends: this.state.friends.concat({ userEmail: name })
-            }
-        })
-    }
-
-    /* event handler for removing a friend */ 
-    handleRemoveFriend (name) {
-        console.log("Removed " + name);
-        // filter out the names excluding the removed user
-        this.setState((name) => {          
-          return {
-              friends: this.state.friends.filter((friend) => friend.userEmail !== name)
-          }
-        })
-    }
-  
-    /* render the friends list */
-    render() {
-        const { friends } = this.state;
-        return (
-            <div>
-                <h1>Friend Page</h1>
-                {/* lists the friends of the user */}
-                {this.state.friends.map((name) => 
-                    this.viewFriend(name.userEmail)
-                ).join('\n')}; {/* not sure how to have each friend on different line */}
-            </div>
-        )
-    }
 }
+
+function Friend({ email, compatibility }) {
+    return (
+        <div>
+            <h2>{UserService.getUser(email)}</h2>
+            <p>{UserService.getBiography(email)}</p>
+            <h3>{compatibility}</h3>
+        </div>
+    );
+};
