b1d8f3f7d7519e6a8b2e2382bc1f02d2c8ea8895
Alan Tsui <alantsui18@gmail.com>
2020-11-27 18:36:45 -0800

    started edit profile

--- client/src/components/SocialMedia.js
+++ client/src/components/SocialMedia.js
@@ -11,62 +11,7 @@ class SocialMedia extends Component {
       instagram: 'https://www.instagram.com',
       twitter: 'https://www.twitter.com',
     };
-    // change functions
-    this.changeFacebook = this.changeFacebook.bind(this);
-    this.changeInstagram = this.changeInstagram.bind(this);
-    this.changeTwitter = this.changeTwitter.bind(this);
   }
-
-  // check mount
-  componentDidMount() {
-    this.getSocials();
-  }
-
-  /* NOT FINISHED */
-  getSocials() {
-    //let socials = UserService.getSocials();
-    //this.setState();
-  }
-
-  // change facebook URL
-  changeFacebook(facebookURL) {
-    this.setState({ facebook: facebookURL });
-    /* USER SERVICE DOESNT WORK FOR NOW
-        UserService.update({ facebook: this.state.facebook })
-          .then(() => {
-            console.log("Updated Facebook URL succesfully!");
-          })
-          .catch((e) => {
-            console.log(e);
-          });*/
-  }
-
-  // change instagram URL
-  changeInstagram(instagramURL) {
-    this.setState({ instagram: instagramURL });
-    /* USER SERVICE DOESNT WORK FOR NOW
-        UserService.update({ instagram: this.state.instagram })
-          .then(() => {
-            console.log("Updated Instagram URL succesfully!");
-          })
-          .catch((e) => {
-            console.log(e);
-          });*/
-  }
-
-  // change twitter URL
-  changeTwitter(twitterURL) {
-    this.setState({ twitter: twitterURL });
-    /* USER SERVICE DOESNT WORK FOR NOW
-        UserService.update({ twitter: this.state.twitter })
-          .then(() => {
-            console.log("Updated Twitter URL succesfully!");
-          })
-          .catch((e) => {
-            console.log(e);
-          });*/
-  }
-
   // render functionn
   render() {
     return (
--- client/src/pages/EditProfilePage/index.js
+++ client/src/pages/EditProfilePage/index.js
@@ -7,6 +7,9 @@ class EditProfilePage extends Component {
     // set default state of text element instead text box
     this.state = {
       Biography: 'Edit your biography!',
+      facebook: 'https://www.facebook.com',
+      instagram: 'https://www.instagram.com',
+      twitter: 'https://www.twitter.com',
     };
     //event handlers for when we update text field and submit button
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -14,34 +17,75 @@ class EditProfilePage extends Component {
 
   //checks if component mounted
   componentDidMount() {
+    console.log('mounted ;)');
     this.getBiography();
+    this.getFacebook();
+    this.getInstagram();
+    this.getTwitter();
   }
+
+  // GET STUFF NEEDS TO BE UPDATED WITH USERSERVICE
 
   getBiography() {
     //var bioText = UserService.getBiography();
-    var bioText = 'place holder for UserService';
+    let bioText = 'place holder for UserService';
     this.setState({ Biography: bioText });
   }
 
-  /*event handler for when our text field changes so we update this.state.value
-              and text box to reflect change*/
+  getFacebook() {
+    let facebookURL = 'https://www.facebook.com/';
+    this.setState({ facebook: facebookURL });
+  }
+
+  getInstagram() {
+    let instagramURL = 'https://www.instagram.com';
+    this.setState({ instagram: instagramURL });
+  }
+
+  getTwitter() {
+    let twitterURL = 'https://www.twitter.com';
+    this.setState({ twitter: twitterURL });
+  }
+
   /* event handler for when user hits submit button*/
   handleSubmit(event) {
     //bioText = this.state.value;
     this.setState({ Biography: event.target.Biography });
-    alert('Stuff is updated updated: ' + this.state.Biography);
+    alert('Stuff is updated updated');
     event.preventDefault();
   }
   /* TODO: edit social media, edit profile picture, featured artist and track*/
-  
+
   /* render text box*/
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Biography:
-          <textarea value={this.state.Biography} />{' '}
-        </label>{' '}
+        <div id='biography'>
+          <label>
+            Biography:
+            <textarea value={this.state.Biography} />{' '}
+          </label>{' '}
+        </div>
+        <div id='socials'>
+          <div id='facebook'>
+            <label>
+              Facebook:
+              <textarea value={this.state.facebook} />{' '}
+            </label>{' '}
+          </div>
+          <div id='instagram'>
+            <label>
+              Instagram:
+              <textarea value={this.state.instagram} />{' '}
+            </label>{' '}
+          </div>
+          <div id='twitter'>
+            <label>
+              Twitter:
+              <textarea value={this.state.twitter} />{' '}
+            </label>{' '}
+          </div>
+        </div>
         <input type='submit' value='Submit' />{' '}
       </form>
     );
