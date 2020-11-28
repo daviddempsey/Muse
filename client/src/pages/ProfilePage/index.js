be1eb1c8fd37948793a5ac2c2149979117d350ef
Edward Yau <edyau@ucsd.edu>
2020-11-28 14:29:35 -0800

    Moved profile page assets and components into profile page folder

--- client/src/pages/ProfilePage/index.js
+++ client/src/pages/ProfilePage/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import HeaderIcon from "../../assets/headericon.png";
+import HeaderIcon from "./assets/headericon.png";
 import "./index.css";
-import ProfilePicture from "../../components/ProfilePicture";
-import Biography from "../../components/Biography";
-import CurrentTrack from "../../components/CurrentTrack";
-import FeaturedArtist from "../../components/FeaturedArtist";
-import FeaturedTrack from "../../components/FeaturedTrack";
-import ProfileLink from "../../components/ProfileLink";
-import SocialMedia from "../../components/SocialMedia";
-import SpotifyStats from "../../components/SpotifyStats";
+import ProfilePicture from "./components/ProfilePicture";
+import Biography from "./components/Biography";
+import CurrentTrack from "./components/CurrentTrack";
+import FeaturedArtist from "./components/FeaturedArtist";
+import FeaturedTrack from "./components/FeaturedTrack";
+import ProfileLink from "./components/ProfileLink";
+import SocialMedia from "./components/SocialMedia";
+import SpotifyStats from "./components/SpotifyStats";
 
 const ProfilePage = () => {
   return (
