0323ef6c8a603943e2418946cf6b56a09732a3ec
William Duan <38791932+duanwilliam@users.noreply.github.com>
2020-11-28 17:05:37 -0800

    add profile page layout

---
+++ client/src/pages/ProfilePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+import DefaultLayout from '../DefaultLayout';
+
+const ProfilePage = () => {
+  return <div className="ProfilePage">
+    <DefaultLayout>
+      
+    </DefaultLayout>
+  </div>;
+};
+export default ProfilePage;
