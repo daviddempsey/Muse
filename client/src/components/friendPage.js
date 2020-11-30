1dd0b45873c98a6a57ec7a946d9ef1f383b6023f
enochli5907 <enochli5907@gmail.com>
2020-11-29 17:01:17 -0800

    Created FriendsPage page

---
+++ client/src/components/friendPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import UserService from "../services/user.service"
+
+/* Make friends list a functional component
+ * Have a friendsPage component that 
+
+/* view and a user's friends list */
+export default function friendPage() {
+    // get the friends list from the database
+    let friendsList = UserService.getFriends();
+
+    const friends = friendsList.map((email, compatibility) => {
+        return <li><Friend email={email, compatibility}/></li>
+    });
+    
+    return (
+        <div>
+            {friends}
+        </div> 
+    )
+}
+
+function Friend({ email, compatibility }) {
+    return (
+        <div>
+            <h2>{UserService.getUser(email)}</h2>
+            <p>{UserService.getBiography(email)}</p>
+            <h3>{compatibility}</h3>
+        </div>
+    );
+};
