ea45a7488deba2800dbc9f2cd58e5f6c8c9dcfb5
Kenny Yu <kennyyu168@yahoo.com>
2020-12-01 13:29:00 -0800

    added correct syntax to user services file

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,47 +1,152 @@
-//import firebase from 'firebase';
 import fb from '../base.js';
-//const db = firebase.firestore();
+
 const userCollection = fb.firestore().collection('user');
 const profileCollection = fb.firestore().collection('profile');
-// we probably need to add more collections since we only reference user rn when we need
-// to reference profile too
+const statsCollection = fb.firestore().collection('stats');
 
 class UserService {
-    getAll() {
-        return userCollection.get();
+    /* PROFILE EDIT FUNCTIONS */
+    async setBiography(email, bio) {
+      try {
+        // get the document to be changed
+        const bioDoc = await profileCollection.doc(email);
+        await bioDoc.update({
+          biography: bio
+        });
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
     }
 
-    getUser(email) {
-        return userCollection.doc(email);
-    }
-
-    /*  getProfilePicture(email) {
-           return userCollection.doc(email).get()['profile_picture'];
-
-           getFacebook(email) {
-        Promise.resolve('Success').then
-        return profileCollection.doc(email).get().then(
-            (res) => {
-                let facebook = res.data()['social_media']['facebook'];
-                console.log(facebook);
-                return facebook;
-            }
-        );
-    }
-       } */
-
-    /* SOCIAL MEDIA GET FUNCTIONS */
-    /*async getFacebook(email) {
-        return new Promise((resolve, reject) => {
-            profileCollection.doc(email).get().data()['social_media']['facebook']
-                .then(res => {
-                    resolve(res)
-                }, err => {
-                    reject(err)
-                })
-        })
+    /*async setPlaylist(email, playlist) {
+      
     }*/
 
+    /* SOCIAL MEDIA EDIT FUNCTIONS */
+    async setFacebook(email, facebookLink) {
+      try {
+        // get the document to be changed
+        const profileDoc = profileCollection.doc(email);
+        await profileDoc.update({
+          "social_media.facebook": facebookLink
+        });
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async setInstagram(email, instaLink) {
+      try {
+        // get the document to be changed
+        const profileDoc = profileCollection.doc(email);
+        await profileDoc.update({
+          "social_media.instagram": instaLink
+        });
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async setTwitter(email, twitterLink) {
+      try {
+        // get the document to be changed
+        const profileDoc = profileCollection.doc(email);
+        await profileDoc.update({
+          "social_media.twitter": twitterLink
+        });
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async setTiktok(email, tiktokLink) {
+      try {
+        // get the document to be changed
+        const profileDoc = profileCollection.doc(email);
+        await profileDoc.update({
+          "social_media.tiktok": tiktokLink
+        });
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    /* DOCUMENT GET FUNCTIONS */
+    async getAll() {
+      try {
+        const response = await userCollection.get();
+        const allUsers = response.data();
+        return allUsers;
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async getUser(email) {
+      try {
+        const response = await userCollection.doc(email);
+        const userDoc = await response.get();
+        return userDoc.data();
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async getUserProfile(email) {
+      try {
+        const response = await profileCollection.doc(email);
+        const userProfileDoc = await response.get();
+        return userProfileDoc.data();
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    /* PROFILE INFORMATION GET FUNCTIONS */
+    async getBiography(email) {
+      try {
+        const response = await profileCollection.doc(email);
+        const biography = await response.get();
+        return biography.data()['biography'];
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async getProfilePicture(email) {
+      // try getting the profile picture
+      try {
+        const response = await profileCollection.doc(email);
+        const profilePic = await response.get();
+        return profilePic.data()['profile_picture'];
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    } 
+
+    async getProfileLink(email) {
+      // try getting the profile link
+      try {
+        const response = await profileCollection.doc(email);
+        const profileLink = await response.get();
+        return profileLink.data()['profile_link'];
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    /* SOCIAL MEDIA GET FUNCTIONS */
     async getFacebook(email) {
         try {
             const response = await profileCollection.doc(email);
@@ -75,39 +180,67 @@ class UserService {
         }
     }
 
-    getTikTok(email) {
-            return profileCollection.doc(email).get()['social_media'];
-        }
-        /* 
-                  getSpotify(email) {
-                      return profileCollection.doc(email).get()['spotify'];
-                  } */
+    async getTikTok(email) {
+      try {
+          const response = await profileCollection.doc(email);
+          const tiktokData = await response.get();
+          console.log(tiktokData.data()['social_media']['tiktok']);
+          return tiktokData.data()['social_media']['tiktok'];
+      } catch (error) {
+          console.log(error);
+      }
+    }
+
+    async getSpotify(email) {
+      try {
+        const response = await profileCollection.doc(email);
+        const spotifyData = await response.get();
+        console.log(spotifyData.data()['social_media']['spotify']);
+        return spotifyData.data()['social_media']['spotify'];
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    /* STATS INFORMATION GET FUNCTIONS */
+    async getTopArtists(email) {
+      try {
+        const response = await statsCollection.doc(email);
+        const topArtists = await response.get();
+        return topArtists.data()['top_artists'];
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async getTopGenres(email) {
+      try {
+        const response = await statsCollection.doc(email);
+        const topGenres = await response.get();
+        return topGenres.data()['top_genres'];
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    async getTopTracks(email) {
+      try {
+        const response = await statsCollection.doc(email);
+        const topTracks = await response.get();
+        return topTracks.data()['top_tracks'];
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
+
+    /* SPOTIFY REAL TIME DATA FETCH */
 
     /* Profile information get functions */
-    /* getBiography(email) {
-          return userCollection.doc(email).get()['biography'];
-      } */
-
-    /*   getProfileLink(email) {
-            return userCollection.doc(email).get()['profile_picture'];
-        }
-
-        getTopArtists(email) {
-            return userCollection.doc(email).get()['top_artists'];
-        }
-
-        getTopGenres(email) {
-            return userCollection.doc(email).get()['top_genres'];
-        }
-
-        getTopTracks(email) {
-            return userCollection.doc(email).get()['top_tracks'];
-        }
-
-        getUserPlaylists(email) {
-            return userCollection.doc(email).get()['playlists'];
-        } 
-
+    /*
         getSongStats() {
             // NO SONG STATS IN DATABASE YET
         }
@@ -119,10 +252,12 @@ class UserService {
         }
         getMinuteStats() {
             // NO MINUTES STATS IN DATABASE YET
-        }*/
+        }
+    */
 
     // what is this function
-    /*create(user) {
+    /* 
+        create(user) {
           return db.add(user);
         }
 
@@ -134,7 +269,8 @@ class UserService {
         // i don't think we can delete this email
         delete(email) {
           return db.doc(email).delete();
-        }*/
+        }
+    */
 }
 
 export default new UserService();
