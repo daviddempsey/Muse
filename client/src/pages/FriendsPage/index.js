78d94987844a926769fefe473e8474c2383d5f57
enochli5907 <enochli5907@gmail.com>
2020-12-02 01:17:15 -0800

    Reorganized to match master branch

--- client/src/pages/FriendsPage/index.js
+++ client/src/pages/FriendsPage/index.js
@@ -1,28 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './index.css';
 
 
 import DefaultLayout from '../DefaultLayout';
 import FriendsPage from '../../components/friendPage';
 
-export default class FriendPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  componentDidMount = () => {};
-
-  render = () => {
-    return (
-      <div id='friend-page'>
-        <DefaultLayout>
-          <div id='friends'>
-            <FriendsPage />
-          </div>
-        </DefaultLayout>
-      </div>
-    )
-  }
+export default function FriendPage() {
+  return (
+    <div id='friend-page'>
+      <DefaultLayout>
+        <div id='friends'>
+          <FriendsPage />
+        </div>
+      </DefaultLayout>
+    </div>
+  );
 }
