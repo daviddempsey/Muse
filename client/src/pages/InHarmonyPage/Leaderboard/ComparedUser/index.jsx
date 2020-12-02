f4d4585af32c37fd569c70807217602bdcfe6e74
David Dempsey <davidgdempsey@gmail.com>
2020-12-01 21:59:15 -0800

    Added GET and POST requests to backend for leaderboard

--- client/src/pages/InHarmonyPage/Leaderboard/ComparedUser/index.jsx
+++ client/src/pages/InHarmonyPage/Leaderboard/ComparedUser/index.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 const ComparedUser = () => {
   return (
     <div className="ComparedUser">
-        
     </div>
   );
 };
--- client/src/pages/InHarmonyPage/Leaderboard/index.jsx
+++ client/src/pages/InHarmonyPage/Leaderboard/index.jsx
@@ -1,20 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+// import firebase from 'firebase/app';
+// import fb from '../../base';
+// import 'firebase/firestore';
+// import 'firebase/auth';
 // import './index.css';
 
 class Leaderboard extends Component {
     constructor(props) {
         super(props);
-        let topUsers = [];
+        this.email = props.email;
+        this.state = {
+            topUsers: []
+        };
+        this.setTopUsers = this.setTopUsers.bind(this);
     }
 
-    compareUsers = (user) => {
-        
+    getTopUsers = () => {
+        const url = "http://localhost:5001/muse-eec76/us-central1/app/compareUsers?"
+        const options = {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            body: JSON.stringify({
+                email: email
+            })
+        };
+        fetch(url, options)
+            .then(response => {
+                this.setTopUsers(response.data.topUsers);
+                console.log(response.status);
+            });
+    }
+
+    compareUsers = () => {
+        const url = "http://localhost:5001/muse-eec76/us-central1/app/compareUsers?"
+        const options = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            body: JSON.stringify({
+                email: email
+            })
+        };
+        fetch(url, options)
+            .then(response => {
+                console.log(response.status);
+                this.getTopUsers();
+            });
+    }
+
+    componentWillMount() {
+        this.getTopUsers();
     }
 
     render() {
         return (
             <div className="Leaderboard">
-
+                {topUsers.map(user => <ComparedUser user={user.email} />)}
             </div>
         );
     }
