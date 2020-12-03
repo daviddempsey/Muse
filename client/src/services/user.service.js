21cc3848ea15f8853acd93e5d5a179cbd2401592
Alan Tsui <alantsui18@gmail.com>
2020-12-02 22:21:14 -0800

    unified file formattings in client/

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,77 +1,77 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const db = firebase.firestore();
-const userCollection = db.collection("user");
+const userCollection = db.collection('user');
 
 class UserService {
-  getAll() {
-    return userCollection.get();
-  }
+    getAll() {
+        return userCollection.get();
+    }
 
-  getUser(email) {
-    return userCollection.doc(email);
-  }
+    getUser(email) {
+        return userCollection.doc(email);
+    }
 
-  getProfilePicture(email) {
-    return userCollection.doc(email).get()["profile_picture"];
-  }
+    getProfilePicture(email) {
+        return userCollection.doc(email).get()['profile_picture'];
+    }
 
-  /* SOCIAL MEDIA GET FUNCTIONS */
-  getFacebook(email) {
-    return userCollection.doc(email).get()["facebook"];
-  }
+    /* SOCIAL MEDIA GET FUNCTIONS */
+    getFacebook(email) {
+        return userCollection.doc(email).get()['facebook'];
+    }
 
-  getInstagram(email) {
-    return userCollection.doc(email).get()["instagram"];
-  }
+    getInstagram(email) {
+        return userCollection.doc(email).get()['instagram'];
+    }
 
-  getTwitter(email) {
-    return userCollection.doc(email).get()["twitter"];
-  }
+    getTwitter(email) {
+        return userCollection.doc(email).get()['twitter'];
+    }
 
-  getTikTok(email) {
-    return userCollection.doc(email).get()["tiktok"];
-  }
+    getTikTok(email) {
+        return userCollection.doc(email).get()['tiktok'];
+    }
 
-  getSpotify(email) {
-    return userCollection.doc(email).get()["spotify"];
-  }
+    getSpotify(email) {
+        return userCollection.doc(email).get()['spotify'];
+    }
 
-  /* Profile information get functions */
-  getBiography(email) {
-    return userCollection.doc(email).get()["biography"];
-  }
+    /* Profile information get functions */
+    getBiography(email) {
+        return userCollection.doc(email).get()['biography'];
+    }
 
-  getProfileLink(email) {
-    return userCollection.doc(email).get()["profile_picture"];
-  }
+    getProfileLink(email) {
+        return userCollection.doc(email).get()['profile_picture'];
+    }
 
-  getTopArtists(email) {
-    return userCollection.doc(email).get()["top_artists"];
-  }
+    getTopArtists(email) {
+        return userCollection.doc(email).get()['top_artists'];
+    }
 
-  getTopGenres(email) {
-    return userCollection.doc(email).get()["top_genres"];
-  }
+    getTopGenres(email) {
+        return userCollection.doc(email).get()['top_genres'];
+    }
 
-  getTopTracks(email) {
-    return userCollection.doc(email).get()["top_tracks"];
-  }
+    getTopTracks(email) {
+        return userCollection.doc(email).get()['top_tracks'];
+    }
 
-  // what is this function 
-  /*create(user) {
-    return db.add(user);
-  }
+    // what is this function
+    /*create(user) {
+      return db.add(user);
+    }
 
-  // I don't think email can be changed
-  update(email, value) {
-    return db.doc(email).update(value);
-  }
+    // I don't think email can be changed
+    update(email, value) {
+      return db.doc(email).update(value);
+    }
 
-  // i don't think we can delete this email
-  delete(email) {
-    return db.doc(email).delete();
-  }*/
+    // i don't think we can delete this email
+    delete(email) {
+      return db.doc(email).delete();
+    }*/
 }
 
 export default new UserService();
