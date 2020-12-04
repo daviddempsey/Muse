e56d44da90633db9c49cbbd6dffe0b0ae9689b99
kennyyu168 <kennyyu168@yahoo.com>
2020-12-03 23:53:00 -0800

    fixed conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import FriendsPage from './pages/FriendsPage';
+import ProfilePage from './pages/ProfilePage';
+import EditProfilePage from './pages/EditProfilePage';
+import LoadingPage from './pages/LoadingPage';
 import './styles/index.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/friends" component={FriendsPage} />
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/profile' component={ProfilePage} />
+          <Route exact path='/editprofile' component={EditProfilePage} />
+          <Route exact path='/logging' component={LoadingPage} />
         </Switch>
       </Router>
     </div>
--- client/src/base.js
+++ client/src/base.js
@@ -1,14 +1,16 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 
 var fb = firebase.initializeApp({
-    apiKey: "AIzaSyCXHNEOM_Y2rdIC3kvncBsYd7gAf88HJ6g",
-    authDomain: "muse-eec76.firebaseapp.com",
-    databaseURL: "https://muse-eec76.firebaseio.com",
-    projectId: "muse-eec76",
-    storageBucket: "muse-eec76.appspot.com",
-    messagingSenderId: "1076771976447",
-    appId: "1:1076771976447:web:41acb692f55285ef9a08ca",
-    measurementId: "G-3D6DCMR4W7"
+    apiKey: 'AIzaSyCXHNEOM_Y2rdIC3kvncBsYd7gAf88HJ6g',
+    authDomain: 'muse-eec76.firebaseapp.com',
+    databaseURL: 'https://muse-eec76.firebaseio.com',
+    projectId: 'muse-eec76',
+    storageBucket: 'muse-eec76.appspot.com',
+    messagingSenderId: '1076771976447',
+    appId: '1:1076771976447:web:41acb692f55285ef9a08ca',
+    measurementId: 'G-3D6DCMR4W7',
 });
+
+//var db = firebase.firestore();
 
 export default fb;
