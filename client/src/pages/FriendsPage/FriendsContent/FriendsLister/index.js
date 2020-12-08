075530a6cadf06b3eb069558ab4864a691578bde
Vincent Santos <vincentsantoes@gmail.com>
2020-12-07 23:41:03 -0800

    Fixed the expansion for friends list LES GO

--- client/src/pages/FriendsPage/FriendsContent/FriendsLister/index.js
+++ client/src/pages/FriendsPage/FriendsContent/FriendsLister/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Friend from "../Friend";
-import "./index.css";
 
  /* calls on a Friend component for each friend in the user's friendsList */
  const FriendsLister = ({ friendsList }) =>
