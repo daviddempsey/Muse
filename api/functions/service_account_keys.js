a1a4b1d7dbf00c12ad7c8ff758ec3ef802cfc32d
David Dempsey <davidgdempsey@gmail.com>
2020-12-08 15:44:57 -0800

    Fixed unnecessary gitignore line

---
+++ api/functions/service_account_keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": process.env.GOOGLE_PROJECT_ID,
+  "private_key_id": process.env.GOOGLE_KEY_ID,
+  "private_key": process.env.GOOGLE_PRIVATE_KEY,
+  "client_email": process.env.GOOGLE_CLIENT_EMAIL,
+  "client_id": process.env.GOOGLE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-pz3yl%40muse-eec76.iam.gserviceaccount.com"
+};
