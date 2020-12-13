a51adb61ad41de62ccda6c80e4e3bb5f266da552
Edward Yau <edyau@ucsd.edu>
2020-12-12 17:01:07 -0800

    Fixed warnings and formatting

--- client/src/pages/ChatroomPage/MessagingList/MessagingListItem/index.js
+++ client/src/pages/ChatroomPage/MessagingList/MessagingListItem/index.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import UserService from '../../../../services/user.service';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import UserService from "../../../../services/user.service";
 import "./index.css";
 
-const MessagingListItem = ({email, receiver}) => {
+const MessagingListItem = ({ email, receiver }) => {
   const [name, setName] = useState("");
   const [pfp, setPfp] = useState("");
   const [bio, setBio] = useState("");
@@ -18,7 +18,7 @@ const MessagingListItem = ({email, receiver}) => {
 
   const getBio = async (email) => {
     setBio(await UserService.getBiography(email));
-  }
+  };
 
   React.useEffect(() => {
     getName(email);
@@ -26,18 +26,23 @@ const MessagingListItem = ({email, receiver}) => {
     getBio(email);
   });
 
-  return <div className="MessagingListItem" style={{background: receiver == email ? "#e5e5e5" : "#fcfcfc"}}>
-    <Link to={"/messages/" + btoa(email)}>
-      <div className="padding">
-        <div className="pfp">
-          <img src={pfp} alt="pfp" />
+  return (
+    <div
+      className="MessagingListItem"
+      style={{ background: receiver === email ? "#e5e5e5" : "#fcfcfc" }}
+    >
+      <Link to={"/messages/" + btoa(email)}>
+        <div className="padding">
+          <div className="pfp">
+            <img src={pfp} alt="pfp" />
+          </div>
+          <div className="text">
+            <h2>{name}</h2>
+            <p>{bio}</p>
+          </div>
         </div>
-        <div className="text">
-          <h2>{name}</h2>
-          <p>{bio}</p>
-        </div>
-      </div>
-    </Link>
-  </div>;
+      </Link>
+    </div>
+  );
 };
 export default MessagingListItem;
